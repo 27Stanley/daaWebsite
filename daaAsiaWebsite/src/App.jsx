b1939d2acd0827106df78701458b1ee654d766bc
Stanley <27.stanleylch@gmail.com>
2024-03-09 07:05:34 +0800

    initialise react and picocss as well as implement global font style

---
+++ daaAsiaWebsite/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Index";
+import GlobalStyle from "./assets/GlobalStyle";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ daaAsiaWebsite/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <button>News and Events</button>
+        <button>Discover Dental</button>
+        <button>Education</button>
+        <button>About Us</button>
+      </header>
+    </div>
+  );
+}
