7760ce28c4ea85c8cb8f4f69ce30a1012127ef90
Stanley <27.stanleylch@gmail.com>
2024-03-09 08:30:19 +0800

    will add some boilerplate and gif animations linking to daa

--- daaAsiaWebsite/src/pages/Index.jsx
+++ daaAsiaWebsite/src/pages/Index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import Introduction from "./Introduction";
 
 export default function Index() {
   return (
     <div>
       <Header />
+      <Introduction />
     </div>
   );
 }
---
+++ daaAsiaWebsite/src/pages/Introduction.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Introduction() {
+  return (
+    <div>
+      <h1>daa-introduction here</h1>
+      <div>
+        {React.createElement("dotlottie-player", {
+          src: "https://lottie.host/a4bf4c43-b7eb-41dd-adda-1c01b6e5bafc/SJlNhdpXxW.json",
+          background: "transparent",
+          speed: "1",
+          style: { width: "600px", height: "600px" },
+          loop: true,
+          autoplay: true,
+        })}
+      </div>
+      <div>
+        {React.createElement("dotlottie-player", {
+          src: "https://lottie.host/e5fceedf-f4af-4c87-854f-750f216bbe27/wS2UMv2OeC.json",
+          background: "transparent",
+          speed: "1",
+          style: { width: "600px", height: "600px" },
+          loop: true,
+          autoplay: true,
+        })}
+      </div>
+
+      <div>
+        {React.createElement("dotlottie-player", {
+          src: "https://lottie.host/29b54e02-0cc7-4ad8-9dbd-6919de060f4e/THENfQNgX3.json",
+          background: "transparent",
+          speed: "1",
+          style: { width: "600px", height: "600px" },
+          loop: true,
+          autoplay: true,
+        })}
+      </div>
+    </div>
+  );
+}
